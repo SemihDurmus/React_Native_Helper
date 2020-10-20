25f52eff330718798124318b2ebaa4adee4ae293
SemihDurmus <semihdurmus@yahoo.com>
2020-10-20 22:01:05 +0200

    Add: Navigation files

--- workSpace/src/pages/First.js
+++ workSpace/src/pages/First.js
@@ -1,13 +1,35 @@
-import react from 'react';
-import {SafeAreView, View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {SafeAreaView, View, Text, Button, TextInput} from 'react-native';
+
+const number = 15;
 
 const First = (props) => {
+  const [userName, setUserName] = useState('');
   return (
-    <SafeAreView>
+    <SafeAreaView>
       <View>
-        <Text>First</Text>
+        <Text style={{fontSize: 40}}>First</Text>
+        <Text style={{fontSize: 40}}>{number}</Text>
+        <Text style={{fontSize: 40}}>Entered value is : {userName}</Text>
+        <View style={{backgroundColor: '#e0e0e0', padding: 10, margin: 15}}>
+          <TextInput
+            placeholder={'Enter a text'}
+            value={userName}
+            onChangeText={(text) => setUserName(text)}
+          />
+        </View>
+        <Button
+          title={'GO'}
+          onPress={
+            () =>
+              props.navigation.navigate('SecondPage', {
+                myNumber: number,
+                selectedValue: userName,
+              }) //sending a variable to another page
+          }
+        />
       </View>
-    </SafeAreView>
+    </SafeAreaView>
   );
 };
 
