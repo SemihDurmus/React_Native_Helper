7d4789810f64137a6e1d22d78fb4ed253ee96fd7
SemihDurmus <semihdurmus@yahoo.com>
2020-10-20 22:49:34 +0200

    Add: Documentation

---
+++ workSpace/z_20.10_navigation/src/Router.js
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import {View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import First from './pages/First';
+import Second from './pages/Second';
+
+const Stack = createStackNavigator();
+
+function Router() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{headerShown: false, gestureEnabled: false}}>
+        <Stack.Screen
+          name="FirstPage"
+          component={First}
+          options={{title: 'Outbox'}}
+        />
+        <Stack.Screen name="SecondPage" component={Second} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default Router;
